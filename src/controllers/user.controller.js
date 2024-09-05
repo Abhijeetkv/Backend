94951f5067e1ec09277f6f28c21c0b9e2f5f9677
Abhijeet kumar <abhijeetkv2@gmail.com>
2024-09-06 00:42:49 +0530

    cookies done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -129,10 +129,56 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
 
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200,
+            {
+                user: loggedInUser, 
+                accessToken,
+                refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+
+})
+
+
+
+const logoutUser = asyncHandler(async (req, res) => {
+        await User.findByIdAndUpdate(
+
+            req.user._id,
+            {
+                 $set: { refreshToken: undefined } 
+            },
+            { 
+                new: true
+            }
+        )
+
+        const options = {
+            httpOnly: true,
+            secure: true,
+        }
+
+        return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged out successfully"))
 })
 
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,4 +19,11 @@ router.route("/register").post(
 );
 
 
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(veriftyJWT, logoutUser)
+
+
+
 export default router;
