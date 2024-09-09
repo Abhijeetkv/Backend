f283afdbed380e2ec3e91de446d6ed6ea935e393
Abhijeet kumar <abhijeetkv2@gmail.com>
2024-09-10 00:34:40 +0530

    Add getUserChannelProfile functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -359,6 +359,86 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
         new ApiResponse(200, user, "Cover image updated successfully")
     )
  })
+
+
+ const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { userName } = req.params;
+
+    if (!userName?.trim()) {
+        throw new ApiError(400, "Please provide a username");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                userName: userName?.toLowerCase(),
+            }
+        },
+
+        {
+            $lookup : {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers",
+            }
+        },
+
+        {
+            $lookup : {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo",
+            }
+        },
+
+        {
+            $addFields: {
+                subscribersCount : {
+                    $size: "$subscribers"
+                },
+
+                channelSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+
+        {
+            $project: {
+                fullName: 1,
+                userName: 1,
+                subscribersCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel not found");
+    }   
+    
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "Channel profile fetched successfully"
+        ))
+
+ })
     
 
 export {
@@ -371,5 +451,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
+    getUserChannelProfile
     
 }
